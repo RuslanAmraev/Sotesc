8a2090c8e0a2fb32320887c6fc8f5548c8e43de3
RuslanAmraev <46445130+RuslanAmraev@users.noreply.github.com>
2023-01-25 09:16:24 +0600

    1.4

---
+++ sulfurOil.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="robots" content="index, follow"/>
+    <meta name="keywords" content="Техника безопасности при работе с сернистыми нефтями">
+    <link rel="icon" type="image/x-icon" href="/img/favicon.ico">
+    <title>Техника безопасности при работе с сернистыми нефтями Sotesc </title>
+    <link href="./output.css" rel="stylesheet">
+</head>
+<body>
+<?php include './Header.html' ?>
+    <main class="global-width px-2 mx-auto mt-2 font-medium">
+        <!-- bredcrumb -->
+        <div class="bredcrumb flex border-b-2 border-gray-700">
+            <a class="mr-2 hover:text-[#0369A1]" href="/">Главная</a>
+            <p class="mr-2">/</p>
+            <h1>Техника безопасности при работе с сернистыми нефтями</h1>
+        </div>
+        <!-- bredcrumb -->
+        <div>
+            <h2 class="sub-title-3">Техника безопасности при работе с сернистыми нефтями</h2>
+            <p class="mt-2">
+                К выполнению работ по эксплуатации, техническому обслуживанию, и ремонту оборудования для добычи высоковязкой, сернистой нефти допускаются лица, не моложе 18 лет, прошедшие медицинское освидетельствование и не имеющие противопоказаний к выполнению данного вида работ, обученные по специальности, а так же безопасным методам и приемам работы, применению средств индивидуальной защиты, <a href="/firstHelp" class="link">правилам и приемам оказания первой медицинской помощи</a> пострадавшим. Персонал обучается и сдает экзамен по вопросам промышленной безопасности.
+            </p>
+            <h2 class="sub-title-3">После обучения</h2>
+            <p>Прошедшим обучение, успешно освоившим программу, выдается или удостоверение установленного образца или диплом о профессиональной переподготовке – в зависимости от пройденной программы, а также выписка из протокола.</p>
+            <p class="mt-2">Весь персонал обучается приемам <a href="/firstHelp" class="link">оказания доврачебной помощи</a>. Любой работник, обнаруживший пострадавшего от отравления газами, от ожогов и ранений и других несчастных случаев, оказывает доврачебную помощь пострадавшему. На каждом предприятии с аварийными бригадами проводятся тренировочные занятия с последующей оценкой действий персонала.</p>
+        </div>
+            <div class="mt-2">
+                <h2 class="sub-title">Пройти обучение</h2>
+                <p>
+                    Специализированная компания ТОО "Sotesc" проводит обучение по технике безопасности при работе с сернистыми нефтями компаниям и частным лицам.
+                    Получить консультацию по вопросам пожарной безопасности и уточнить условия заключения договора можно по телефонам:
+                </p>
+                <ul class="mt-2">
+                    <a href="tel:+77076356777" class="address-phone font-semibold flex">
+                        <img class="phone-icon" src="./img/mobile-phone-icon.svg" alt="phone">
+                        <p>+7 707 635 6777</p>
+                    </a>
+                    <a target="_blank" href="https://api.whatsapp.com/send/?phone=77076356777&text&type=phone_number&app_absent=0" class="address-phone font-semibold flex">
+                        <img class="wa-icon" src="./img/wa-logo.webp" alt="message">
+                        <p>+7 707 635 6777</p>
+                    </a>
+                </ul>
+            </div>
+        </div>
+    </main>
+    <?php include './Footer.html' ?>
+</body>
+</html>
+
