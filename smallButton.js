8067fe1fc07d1286f8146923e2bd4635c05ba417
RuslanAmraev <46445130+RuslanAmraev@users.noreply.github.com>
2023-01-26 08:36:43 +0600

    1.5

--- smallButton.js
+++ smallButton.js
@@ -3,8 +3,5 @@ const smallContactButton = document.getElementById('contactButton-small')
 smallContactButton.addEventListener("click",
     ()=>{
         smallContactButton.classList.toggle("contactButton-small-active");
-        if(smallContactButton.classList.contains("contactButton-small-active")){
-            setTimeout(()=>{smallContactButton.classList.toggle("contactButton-small-active")}, 5000)
-        }
     }
 )
