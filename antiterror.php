070297723ead7359be0cd9affac61629eb4e5ce8
RuslanAmraev <46445130+RuslanAmraev@users.noreply.github.com>
2023-02-20 15:39:04 +0600

    3

---
+++ antiterror.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="robots" content="index, follow"/>
+    <meta name="keywords" content="Антитеррористическая защищенность, антитеррор, антитеррористическая защищенность объектов">
+    <link rel="icon" type="image/x-icon" href="/img/logo.webp">
+    <title>Антитеррористическая защищенность Sotesc</title>
+    <meta name="description" content="У нас вы можете пройти профилактику и обучение, получить документацию по Антитеррористической защищенности 'Антитеррор'.">
+    <link href="./output.css" rel="stylesheet">
+</head>
+<body>
+<?php include './Header.html' ?>
+    <main class="global-width px-2 mx-auto mt-2 font-medium">
+        <!-- bredcrumb -->
+        <div class="bredcrumb flex border-b-2 border-gray-700">
+            <a class="mr-2 hover:text-[#0369A1]" href="/">Главная</a>
+            <p class="mr-2">/</p>
+            <h1>Антитеррористическая защищенность</h1>
+        </div>
+        <!-- bredcrumb -->
+        <div>
+            <h2 class="sub-title-3">Антитеррористическая защищенность</h2>
+            <p class="mt-2">
+                Согласно п.16 ст.10-3 Закона РК «О противодействии терроризму» Объекты, уязвимые в террористическом отношении, – особо важные государственные, стратегические, опасные производственные объекты, а также объекты отраслей экономики, имеющие стратегическое значение, объекты массового скопления людей, охраняемые объекты, требующие обязательной организации антитеррористической защиты
+            </p>
+            <p class="mt-2">
+            В соответствии со ст.10-3 Закона РК «О противодействии терроризму» собственники, владельцы, руководители или иные должностные лица объектов, уязвимых в террористическом отношении, независимо от форм собственности обязаны предусмотреть необходимое финансирование и реализовывать мероприятия по организации профилактических и учебных мероприятий с персоналом объектов, по обеспечению их антитеррористической защищенности.
+            </p>
+            <p class="mt-2">
+                Проведение профилактических и учебных мероприятий является необходимым и учитывается при проведении проверок государственными контрольными органами.
+            </p>
+            <h2 class="sub-title-3 mt-2">Результатом завершения обучения является предоставление:</h2>
+            <ul class="list">
+                <li>Сертификат (на организацию от Республиканского центра),</li>
+                <li>Протокол (по обучению персонала технике осмотра помещений, выявлению возможных мест закладки взрывных устройств),</li>
+                <li>Журнал для вновь прибывших сотрудников (с учебной программой для ознакомления).</li>
+            </ul>
+        </div>
+            <div class="mt-2">
+                <h2 class="sub-title">Заказать экспертизу</h2>
+                <p>
+                    Специализированная компания ТОО "Sotesc" проводит обучение по антитеррористической защищенности компаниям и частным лицам.
+                    Получить консультацию и уточнить условия заключения договора можно по телефонам:
+                </p>
+                <ul class="mt-2">
+                    <a href="tel:+77473614690" class="address-phone font-semibold flex">
+                        <img class="phone-icon" src="./img/mobile-phone-icon.svg" alt="phone">
+                        <p>+7 747 361 46 90</p>
+                    </a>
+                    <a target="_blank" href="https://api.whatsapp.com/send/?phone=77473614690&text&type=phone_number&app_absent=0" class="address-phone font-semibold flex">
+                        <img class="wa-icon" src="./img/wa-logo.webp" alt="message">
+                        <p>+7 747 361 46 90</p>
+                    </a>
+                    <a href="tel:+77076356777" class="address-phone font-semibold flex">
+                        <img class="phone-icon" src="./img/mobile-phone-icon.svg" alt="phone">
+                        <p>+7 707 635 6777</p>
+                    </a>
+                    <a target="_blank" href="https://api.whatsapp.com/send/?phone=77076356777&text&type=phone_number&app_absent=0" class="address-phone font-semibold flex">
+                        <img class="wa-icon" src="./img/wa-logo.webp" alt="message">
+                        <p>+7 707 635 6777</p>
+                    </a>
+                </ul>
+            </div>
+        </div>
+    </main>
+    <?php include './Footer.html' ?>
+</body>
+</html>
+
